3cfe899a45112096e0db293a254d1082668d251c
Bhuvan-Agarwal <bhuvangarg123@gmail.com>
2020-01-06 22:05:50 +0530

    Fnction for Verification Of Duplicate Employee ID is Added, & Other Functions Are Updated For Smooth Execution

--- Employee Management.cpp
+++ Employee Management.cpp
@@ -23,7 +23,7 @@ node *insertEmployee(node *);  // For Inserting Details Of A New Employee
 node *searchAndDeleteEmployee(node *,int); // For Deleting or, Removing an Employee From The Organization
 void displayDetails(node *);  // For Displaying The Details Of All The Employee
 void updateDetails(node *,int);  // For Updating Details Of a Particular Employee At a Time
-
+int searchID(node *,int);
 char fake_str[1];
 
 int main()
@@ -76,46 +76,93 @@ int main()
 // Function To Register or, Add Details Of All Employee's Initially
 node *create(node *head)
 {
-	int choice=1;
-	node *new_node,*temp;
-	while(choice)
+	if(head==NULL)
 	{
-		new_node=(node *)malloc(sizeof(node));
-		printf("\nEnter Employee ID:- ");
-		scanf("%d",&new_node->employee.emp_id);
-		gets(fake_str); // Buffer Flusher to get String Input After Taking an Integer Input
-		printf("\nEnter Employee Name:- ");
-		gets(new_node->employee.name);
-		printf("\nEnter Employee Age:- ");
-		scanf("%d",&new_node->employee.age);
-		gets(fake_str); // Buffer Flusher to get String Input After Taking an Integer Input
-		printf("\nEnter The Name of the City where Employee lives:- ");
-		gets(new_node->employee.address.city);
-		printf("\nEnter The Name of the Street where Employee lives:- ");
-		gets(new_node->employee.address.street);
-			
-		new_node->next=NULL;
-			
-		if(head==NULL)
-			head=temp=new_node;
-		else
+		int choice=1,flag=0;
+		node *new_node,*temp;
+		while(choice)
 		{
-			temp->next=new_node;
-			temp=new_node;
+			new_node=(node *)malloc(sizeof(node));
+			printf("\nEnter Employee ID:- ");
+			scanf("%d",&new_node->employee.emp_id);
+			
+			flag=searchID(head,new_node->employee.emp_id);
+			if(flag==1)
+			{
+				printf("\nEmployee ID Already Exists...You Need To Enter Other Employee ID To Register.");
+				continue;
+			}
+			
+			gets(fake_str); // Buffer Flusher to get String Input After Taking an Integer Input
+			printf("\nEnter Employee Name:- ");
+			gets(new_node->employee.name);
+			printf("\nEnter Employee Age:- ");
+			scanf("%d",&new_node->employee.age);
+			gets(fake_str); // Buffer Flusher to get String Input After Taking an Integer Input
+			printf("\nEnter The Name of the City where Employee lives:- ");
+			gets(new_node->employee.address.city);
+			printf("\nEnter The Name of the Street where Employee lives:- ");
+			gets(new_node->employee.address.street);
+				
+			new_node->next=NULL;
+				
+			if(head==NULL)
+				head=temp=new_node;
+			else
+			{
+				temp->next=new_node;
+				temp=new_node;
+			}
+			printf("\nDo You Want To Register Another Employee:- (1/0)? ");
+			scanf("%d",&choice);
 		}
-		printf("\nDo You Want To Register Another Employee:- (1/0)? ");
-		scanf("%d",&choice);
+		return head;
 	}
-	return head;
+	else
+	{
+		printf("\nYou Cannot Again Register Any New Employee Using This Method,,, This Function Is Only To Register Employee Initially");
+		return head;
+	}
+}
+
+// Function To Search & Verify Employee ID Before Inserting ...
+int searchID(node *head,int id)
+{
+	int flag=0;
+	node *temp;
+	temp=head;
+	while(temp!=NULL)
+	{
+		if(temp->employee.emp_id==id)
+		    {
+		    	flag=1;
+		      	break;
+			}
+		else
+			temp=temp->next;
+	}
+	if(flag==1)
+	   return 1;
+	else
+	   return 0;
 }
 
 // Function To Insert or, Add Details Of A New Employee
 node *insertEmployee(node *head)
 {
+	int flag=0;
 	node *new_node;
 	new_node=(node *)malloc(sizeof(node));
 	printf("\nEnter Employee ID:- ");
 	scanf("%d",&new_node->employee.emp_id);
+	
+	flag=searchID(head,new_node->employee.emp_id);
+	if(flag==1)
+		{
+			printf("\nEmployee ID Already Exists...You Need To Enter Other Employee ID To Register.");
+			return head;
+		}
+	
 	gets(fake_str); // Buffer Flusher to get String Input After Taking an Integer Input
 	printf("\nEnter Employee Name:- ");
 	gets(new_node->employee.name);
@@ -191,11 +238,16 @@ void updateDetails(node *head,int id_to_update)
 	int choice;
 	node *temp;
 	temp=head;
-	
-	while(temp->employee.emp_id!=id_to_update)
+	if(head->next==NULL)
+		temp=head;
+	else
 	{
-		temp=temp->next;
+		while(temp!=NULL && temp->employee.emp_id!=id_to_update)
+			{
+				temp=temp->next;
+			}
 	}
+	
 	printf("\n***** Your Existing Details Are *****");
 	printf("\n\tEmployee ID : %d",temp->employee.emp_id);
 	printf("\n\tEmployee Name : %s",temp->employee.name);
@@ -207,16 +259,19 @@ void updateDetails(node *head,int id_to_update)
 	switch(choice)
 	{
 		case 1: printf("\nEnter New Name:- ");
-		        scanf("%s",&temp->employee.name);
+				gets(fake_str);
+		        gets(temp->employee.name);
 		        break;
 	    case 2: printf("\nEnter New Age:- ");
 	            scanf("%d",&temp->employee.age);
 	            break;
 		case 3: printf("\nEnter City Name:- ");
-				scanf("%s",&temp->employee.address.city);
+				gets(fake_str);
+				gets(temp->employee.address.city);
 				break;
 		case 4: printf("\nEnter Street Name:- ");
-				scanf("%s",&temp->employee.address.street);
+				gets(fake_str);
+				gets(temp->employee.address.street);
 				break;
 		case 5: break;
 		   default:
